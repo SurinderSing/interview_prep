1423917e799b04f12f804dcf824c9fe789ef7499
Surinder Singh <ssurindersingh100@gmail.com>
2026-02-06 16:54:34 +0530

    customToLowerCase

--- questions/customToLowerCase.js
+++ questions/customToLowerCase.js
@@ -1,24 +1,40 @@
 const string = "Hello, World!";
 
-const myToLowerCase = (str) => {
+// const myToLowerCase = (str) => {
 
-    var lowerStr = "";
+//     var lowerStr = "";
 
+//     for (let i = 0; i < str.length; i++) {
+//         const elementCharCode = str.charCodeAt(i);
+//         if (elementCharCode >= 65 && elementCharCode <= 90) {
+//             // we will add 35 and convert that to string and then concate
+//             lowerStr += String.fromCharCode(elementCharCode + 32)
+//         }
+//         else {
+//             // we will concate directly
+//             lowerStr += str[i]
+//         }
+
+//     }
+
+//     return lowerStr;
+
+// }
+
+// console.log(myToLowerCase(string));
+
+const customToLowerCase = (str) => {
+    let result = ""
     for (let i = 0; i < str.length; i++) {
-        const elementCharCode = str.charCodeAt(i);
-        if (elementCharCode >= 65 && elementCharCode <= 90) {
-            // we will add 35 and convert that to string and then concate
-            lowerStr += String.fromCharCode(elementCharCode + 32)
+        let element = str[i];
+        if (element.charCodeAt() >= 65 && element.charCodeAt() <= 90) {
+            result += String.fromCharCode(element.charCodeAt() + 32)
+        } else {
+            result += element
         }
-        else {
-            // we will concate directly
-            lowerStr += str[i]
-        }
-
     }
 
-    return lowerStr;
-
+    return result
 }
 
-console.log(myToLowerCase(string));
+console.log(customToLowerCase(string))

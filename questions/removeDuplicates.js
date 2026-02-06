c7810357c096679ffff461862cc42c89acdde0fc
Surinder Singh <ssurindersingh100@gmail.com>
2026-02-06 16:10:04 +0530

    remove duplicates from array

--- questions/removeDuplicates.js
+++ questions/removeDuplicates.js
@@ -1,11 +1,23 @@
-function removeDuplicates(arr) {
-    const newArr = [];
-    for (let item of arr) {
-        if (newArr.indexOf(item) === -1) {
-            newArr.push(item)
-        }
+// function removeDuplicates(arr) {
+//     const newArr = [];
+//     for (let item of arr) {
+//         if (newArr.indexOf(item) === -1) {
+//             newArr.push(item)
+//         }
+//     }
+//     return newArr;
+// }
+
+const removeDuplicates = (ar) => {
+    let result = [];
+
+    for (let i = 0; i < ar.length; i++) {
+        const element = ar[i];
+
+        if (result.indexOf(element) === -1) result.push(element)
+
     }
-    return newArr;
+    return result
 }
 
 const arr = [1, 2, 2, 3, 1, 4]

2bff63c6ac12d01849d60c15fc801a1afc5d3ecd
Surinder Singh <ssurindersingh100@gmail.com>
2026-02-16 16:54:41 +0530

    change

--- questions/kadane_maxsum.js
+++ questions/kadane_maxsum.js
@@ -26,28 +26,54 @@ const array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 // };
 
 // Correct implementation::
-const maxSubArr = (arr) => {
-    let maxSum = arr[0];
-    let currSum = arr[0];
+// const maxSubArr = (arr) => {
+//     let maxSum = arr[0];
+//     let currSum = arr[0];
 
-    let start = 0, tempStart = 0, end = 0;
+//     let start = 0, tempStart = 0, end = 0;
+
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] > currSum + arr[i]) {
+//             currSum = arr[i];
+//             tempStart = i;
+//         } else {
+//             currSum += arr[i];
+//         }
+
+//         if (currSum > maxSum) {
+//             maxSum = currSum;
+//             start = tempStart;
+//             end = i;
+//         }
+//     }
+
+//     return [maxSum, arr.slice(start, end + 1)];
+// };
+
+// console.log(maxSubArr(array));
+
+const maxSubArrSum = (arr) => {
+    let maxSum = arr[0];
+    let currentSum = arr[0];
+    let start = 0,
+        end = 0,
+        temp = 0;
 
     for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > currSum + arr[i]) {
-            currSum = arr[i];
-            tempStart = i;
+        const el = arr[i];
+        if (el > currentSum + el) {
+            currentSum = el;
+            temp = i;
         } else {
-            currSum += arr[i];
+            currentSum = currentSum + el;
         }
-
-        if (currSum > maxSum) {
-            maxSum = currSum;
-            start = tempStart;
+        if (maxSum < currentSum) {
+            maxSum = currentSum;
+            start = temp;
             end = i;
         }
     }
-
-    return [maxSum, arr.slice(start, end + 1)];
+    return { maxSum, start, end };
 };
 
-console.log(maxSubArr(array));
+console.log(maxSubArrSum(array));

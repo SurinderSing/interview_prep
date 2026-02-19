2e23943c505ebb2ca9c9ee8ad81974458173f0a4
Surinder Singh <ssurindersingh100@gmail.com>
2026-02-19 10:48:07 +0530

    feat: implement function to join two sorted arrays

---
+++ questions/join-two-sorted-arrays.js
@@ -0,0 +1,37 @@
+let inputA = [1, 3, 5, 7, 8]
+let inputB = [2, 4, 6, 8, 54, 65]
+
+const joinTwoSortedArrays = (arrA, arrB) => {
+    let i = 0;
+    let j = 0;
+    let result = []
+    while (i < arrA.length || j < arrB.length) {
+        if (i >= arrA.length) {
+            result.push(arrB[j]);
+            j++;
+            continue;
+        }
+
+        if (j >= arrB.length) {
+            result.push(arrA[i]);
+            i++;
+            continue;
+        }
+
+        if (arrA[i] >= arrB[j]) {
+            result.push(arrB[j]);
+            j++;
+            continue;
+        }
+
+        if (arrA[i] <= arrB[j]) {
+            result.push(arrA[i]);
+            i++;
+            continue;
+        }
+
+    }
+    return result
+}
+
+console.log(joinTwoSortedArrays(inputA, inputB))

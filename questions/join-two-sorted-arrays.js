44a409a9a2c3aa4384dc620d7586ca708345499b
Surinder Singh <ssurindersingh100@gmail.com>
2026-02-19 17:42:28 +0530

    feat: update input arrays and improve join logic for sorted arrays

--- questions/join-two-sorted-arrays.js
+++ questions/join-two-sorted-arrays.js
@@ -1,18 +1,18 @@
-let inputA = [1, 3, 5, 7, 8]
-let inputB = [2, 4, 6, 8, 54, 65]
+let inputA = [1, 3, 5, 7, 8, 90]
+let inputB = [2, 4, 6, 8, 54, 65, 102]
 
 const joinTwoSortedArrays = (arrA, arrB) => {
     let i = 0;
     let j = 0;
     let result = []
-    while (i < arrA.length || j < arrB.length) {
-        if (i >= arrA.length) {
+    while (arrA[i] || arrB[j]) {
+        if (!arrA[i]) {
             result.push(arrB[j]);
             j++;
             continue;
         }
 
-        if (j >= arrB.length) {
+        if (!arrB[j]) {
             result.push(arrA[i]);
             i++;
             continue;
@@ -33,5 +33,37 @@ const joinTwoSortedArrays = (arrA, arrB) => {
     }
     return result
 }
+// const joinTwoSortedArrays = (arrA, arrB) => {
+//     let i = 0;
+//     let j = 0;
+//     let result = []
+//     while (i < arrA.length || j < arrB.length) {
+//         if (i >= arrA.length) {
+//             result.push(arrB[j]);
+//             j++;
+//             continue;
+//         }
+
+//         if (j >= arrB.length) {
+//             result.push(arrA[i]);
+//             i++;
+//             continue;
+//         }
+
+//         if (arrA[i] >= arrB[j]) {
+//             result.push(arrB[j]);
+//             j++;
+//             continue;
+//         }
+
+//         if (arrA[i] <= arrB[j]) {
+//             result.push(arrA[i]);
+//             i++;
+//             continue;
+//         }
+
+//     }
+//     return result
+// }
 
 console.log(joinTwoSortedArrays(inputA, inputB))
